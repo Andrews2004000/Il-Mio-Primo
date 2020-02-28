c3c494b2ead03e7b534f97aed5904408eb19bd63
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-02-28 21:50:05 +0100

    Fixed errors

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const Input = document.querySelector("#In");
-const Text = document.querySelector("#Tx");
-const List = document.querySelector("#list");
-const Btn = document.querySelector("#Bt");
-const DeleteBtn = document.querySelector("#delete");
+const Input = document.querySelector('#In');
+const Text = document.querySelector('#Tx');
+const List = document.querySelector('#list');
+const Btn = document.querySelector('#Bt');
+const DeleteBtn = document.querySelector('#delete');
 
-Btn.addEventListener('click', ()=>{
-    var  InputText = Input.value;
-   
-    
-    var  TextValue = Text.value;
-    
-   
-    List.insertAdjacentHTML("afterend", `<li class="item">${InputText}<br>${TextValue}</li>`)
-})
-DeleteBtn.addEventListener("click", ()=>{
+Btn.addEventListener('click', () => {
+    var InputText = Input.value;
+
+    var TextValue = Text.value;
+
+    List.insertAdjacentHTML('beforeend', `<li class="item">${InputText}<br>${TextValue}</li>`);
+});
+DeleteBtn.addEventListener('click', () => {
     List.removeChild();
-})
-
+});
