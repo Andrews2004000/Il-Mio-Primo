d63f6e4da09dff1726dd0d5396b2d43327d3c99e
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-02-28 22:00:15 +0100

    Added delete functionality

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const Input = document.querySelector('#In');
 const Text = document.querySelector('#Tx');
 const List = document.querySelector('#list');
 const Btn = document.querySelector('#Bt');
-const DeleteBtn = document.querySelector('#delete');
-
 Btn.addEventListener('click', () => {
     var InputText = Input.value;
 
     var TextValue = Text.value;
 
-    List.insertAdjacentHTML('beforeend', `<li class="item">${InputText}<br>${TextValue}</li>`);
+    List.insertAdjacentHTML('beforeend', `<li class="item">${InputText}: ${TextValue}</li>`);
 });
-DeleteBtn.addEventListener('click', () => {
-    List.removeChild();
+List.addEventListener('click', event => {
+    const elementLi = event.target.closest('li');
+    List.removeChild(elementLi);
 });
